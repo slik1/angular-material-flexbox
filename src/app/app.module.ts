45641d93e52bc74e07e08477d45b9030b2e85ab1
SLiK <gary@slikinteractive.com>
2019-08-04 14:55:22 -0700

    Created material module and imported it main app module
    Also imported FlexLayoutModule to be able to use it and all Angular Material components throughout app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialModule } from './material-module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
